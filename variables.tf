dd7eb0a420cba6aadcf866a62e07fd86c2c22a17
Mark Kingsbury <mark@kingsburydata.com>
2023-02-03 15:02:38 +0000

    copy of dpsg module tf

---
+++ variables.tf
@@ -0,0 +1,59 @@
+variable "env" {
+  description = "Name of the environment to deploy"
+  type        = string
+}
+
+variable "prefix" {
+  description = "Transformation Engine prefix"
+  type        = string
+}
+
+
+variable "account_id" {
+  description = "Account ID where Image for the Lambda function will be"
+  type        = string
+}
+
+variable "common_tre_slack_alerts_topic_arn" {
+  description = "ARN of the Common TRE Slack Alerts SNS Topic"
+  type        = string
+}
+
+variable "dpsg_version" {
+  description = "DRI Preingest SIP Generation Step Function version (update if Step Function flow or called Lambda function versions change)"
+  type        = string
+
+}
+
+variable "dpsg_image_versions" {
+  description = "Latest version of Images for Lambda Functions"
+  type = object({
+    tre_bagit_to_dri_sip = string
+    tre_sqs_sf_trigger   = string
+  })
+}
+
+variable "common_tre_internal_topic_arn" {
+  description = "The TRE out SNS topic ARN"
+  type        = string
+}
+
+variable "tre_dlq_alerts_lambda_function_name" {
+  description = "TRE DLQ Alerts Lambda Function Name"
+  type        = string
+}
+
+variable "tre_permission_boundary_arn" {
+  description = "ARN of the TRE permission boundary policy"
+  type = string
+}
+
+variable "ecr_uri_host" {
+  description = "The hostname part of the management account ECR repository; e.g. ACCOUNT.dkr.ecr.REGION.amazonaws.com"
+  type = string
+}
+
+variable "ecr_uri_repo_prefix" {
+  description = "The prefix for Docker image repository names to use; e.g. foo/ in ACCOUNT.dkr.ecr.REGION.amazonaws.com/foo/tre-bar"
+  type = string
+}
