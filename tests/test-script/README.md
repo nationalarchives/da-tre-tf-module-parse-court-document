af208be396d5a6637ab175b70de45fab333e9424
Mark Kingsbury <mark@kingsburydata.com>
2023-06-15 08:34:55 +0100

    DTE-744: sf fix naming in status fields + update test script (#19)

--- tests/test-script/README.md
+++ tests/test-script/README.md
@@ -1,12 +1,5 @@
 
-A test msg is sent to trigger the module. THe messge `sns-msg.json` has a docx_url and a uuid substituted before being
+A test msg is sent to trigger the module. The message `sns-msg.json` has a source bucket + source key and a uuid substituted before being
 put on the "sns_arn" topic in the "aws_profile_target".
 
-Use an availaible public docx_url or use optional params "aws_profile_source" & "s3_bucket_source"
-to have one made.
-
-with a public docx url use:
-`./module_sns_msg.sh tre-in-arn tre-non-prod-user s3_object_docx`
-
-or to make a presigned url for the docx (there is one for example in dev-te-testdata) use:
-`./module_sns_msg.sh tre-in-arn tre-non-prod-user da-transform-sample-data/test-judgments/test.docx tre-management-user dev-te-testdata`
+`./module_sns_msg.sh tre-in-arn tre-non-prod-user s3_source_bucket s3_source_key`
--- tests/test-script/module_sns_msg.sh
+++ tests/test-script/module_sns_msg.sh
@@ -2,48 +2,27 @@
 set -e
 
 main() {
-  if [ $# -lt 2 ] || [ $# -gt 5 ]; then
-    echo "Usage: sns_arn aws_profile_target s3_object_docx [aws_profile_source] [s3_bucket_source]"
+  if [ $# -lt 4 ] || [ $# -gt 4 ]; then
+    echo "Usage: sns_arn aws_profile_target s3_bucket_source s3_key_source"
     return 1
   fi
 
   local sns_arn="${1:?}"
   local aws_profile_target="${2:?}"
-  local s3_object_docx="${3:-""}"
-  local aws_profile_source="${4:-""}"
-  local s3_bucket_source="${5:-""}"
-
+  local s3_bucket_source="${3:?}"
+  local s3_key_source="${4:?}"
 
   printf 'sns_arn="%s"\n' "${sns_arn}"
   printf 'aws_profile_target="%s"\n' "${aws_profile_target}"
-  printf 'aws_profile_source="%s"\n' "${aws_profile_source}"
 
-  local docx_url
-  #if no bucket source (and no s3 object docx) then use default, otherwise make a presigned url for docx
-  if [ "$s3_bucket_source" = "" ]
-  then
-          printf "Using Default docx"
-          docx_url=${s3_object_docx}
-# test doc not found with this change
-#          docx_url="https://tre-testing"
-  else
-          local s3_path_docx="s3://${s3_bucket_source}/${s3_object_docx}"
-          printf 'AWS S3 listing for source docx "%s":\n' "${s3_path_docx:?}"
-          aws --profile "${aws_profile_source}" s3 ls "${s3_path_docx}"
-          docx_url="$(aws --profile "${aws_profile_source}" \
-              s3 presign "s3://${s3_bucket_source}/${s3_object_docx}" \
-              --expires-in "${presigned_url_expiry_secs:-60}")"
-  fi
-  printf 'docx_url="%s"\n' "${docx_url}"
-# test presigned url has timedout with a sleep here
-#  sleep 2m
   local test_uuid
   test_uuid="$(uuidgen | tr '[:upper:]' '[:lower:]')"
 
   message_string=$(cat sns-msg.json)
-  message_string_with_url="${message_string/XXX_docx_url_XXX/$docx_url}"
-  message_string_with_url_and_uuid="${message_string_with_url/XXX_uuid_XXX/$test_uuid}"
-  msg_to_publish="${message_string_with_url_and_uuid}"
+  message_string_with_bucket="${message_string/XXX_bucket_XXX/$s3_bucket_source}"
+  message_string_with_bucket_and_key="${message_string_with_bucket/XXX_key_XXX/$s3_key_source}"
+  message_string_with_bucket_and_key_and_uuid="${message_string_with_bucket_and_key/XXX_uuid_XXX/$test_uuid}"
+  msg_to_publish="${message_string_with_bucket_and_key_and_uuid}"
 
   printf 'Publishing Message:\n%s\n' "${msg_to_publish:?}"
   aws --profile "${aws_profile_target:?}" sns publish \
