29dfa9840159214921b8b51cc5ae4c147f9c8ba9
Thanh <tri.lam21@outlook.com>
2024-01-23 15:40:15 +0000

    remove common_tre_slack_alerts_topic_arn

--- sfn.tf
+++ sfn.tf
@@ -3,7 +3,6 @@ resource "aws_sfn_state_machine" "court_document_parse" {
   role_arn = aws_iam_role.court_document_parse.arn
   definition = templatefile("${path.module}/templates/step-function-definition.json.tftpl", {
     arn_lambda_court_document_parse        = aws_lambda_function.court_document_parse.arn
-    arn_sns_topic_tre_slack_alerts   = var.common_tre_slack_alerts_topic_arn
     arn_sns_topic_court_document_parse_out = var.notification_topic_arn
     tre_data_bucket                  = var.tre_data_bucket
   })
